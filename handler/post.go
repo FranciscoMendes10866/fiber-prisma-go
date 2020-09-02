5a36f57f46c7f106787bfccc838ea2ddcde226d2
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-09-03 00:23:47 +0100

    added get many posts handler, find single post with params but the CreatePost handler error persists

--- handler/post.go
+++ handler/post.go
@@ -8,6 +8,60 @@ import (
 	"github.com/gofiber/fiber"
 )
 
+func FindUserPosts(c *fiber.Ctx) {
+	ctx := context.Background()
+	prisma := db.NewClient()
+	err := prisma.Connect()
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := prisma.Disconnect()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	authEmail := claims["email"].(string)
+
+	posts, err := prisma.Post.FindMany(
+		db.Post.Author.Where(
+			db.User.Email.Equals(authEmail),
+		),
+	).Exec(ctx)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(posts)
+}
+
+func FindSinglePost(c *fiber.Ctx) {
+	ctx := context.Background()
+	prisma := db.NewClient()
+	err := prisma.Connect()
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := prisma.Disconnect()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	PostID := c.Params("id")
+
+	post, err := prisma.Post.FindOne(
+		db.Post.ID.Equals(PostID),
+	).Exec(ctx)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(post)
+}
+
 func CreatePost(c *fiber.Ctx) {
 	ctx := context.Background()
 	prisma := db.NewClient()
@@ -36,12 +90,12 @@ func CreatePost(c *fiber.Ctx) {
 	authEmail := claims["email"].(string)
 
 	create, err := prisma.Post.CreateOne(
-		db.Post.Title.Set(body.Title),
-		db.Post.Content.Set(body.Content),
-		db.Post.Published.Set(body.Published),
 		db.Post.Author.Link(
 			db.User.Email.Equals(authEmail),
 		),
+		db.Post.Title.Set(body.Title),
+		db.Post.Content.Set(body.Content),
+		db.Post.Published.Set(body.Published),
 	).Exec(ctx)
 	if err != nil {
 		panic(err)
--- router/router.go
+++ router/router.go
@@ -10,13 +10,16 @@ import (
 func SetupRoutes(app *fiber.App) {
 	app.Use(middleware.Logger())
 	app.Use(middleware.Compress())
+
 	api := app.Group("/api/v1")
+
 	auth := api.Group("/auth")
 	auth.Get("/", guards.AuthRequired(), handler.GetUsers)
 	auth.Post("/create", handler.CreateUser)
 	auth.Post("/login", handler.LoginUser)
 
 	posts := api.Group("/posts")
+	posts.Get("/", guards.AuthRequired(), handler.FindUserPosts)
+	posts.Get("/:id", guards.AuthRequired(), handler.FindSinglePost)
 	posts.Post("/", guards.AuthRequired(), handler.CreatePost)
-
 }
